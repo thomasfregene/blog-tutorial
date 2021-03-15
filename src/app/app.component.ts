beb7b267f3241aff065af06decd80bc3bd4645e8
thomasfregene <fregzythomas@gmail.com>
2021-03-15 07:52:09 +0100

    reusable component with array of objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'blog app';
-  users={
-    name: 'Mark',
-    age: 22,
-    address: 'New York'
-  }
+  users = [
+    {
+      name: 'Mark',
+      age: 22,
+      address: 'New York',
+    },
+    {
+      name: 'Paul',
+      age: 29,
+      address: 'Boston',
+    },
+    {
+      name: 'James',
+      age: 22,
+      address: 'New Mexico',
+    },
+  ];
 }
